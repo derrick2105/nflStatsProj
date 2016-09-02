ed6078e4a0b887c0c03ab84d908f8aec4e8066a4
Derrick <derrick2105@gmail.com>
2016-09-01 22:25:19 -0700

    Refactored the features a bit. I also added some comments of stuff to change, maybe tomorrow before I do the ml stuff.

--- src/ml_classes/FeatureExtractor.py
+++ src/ml_classes/FeatureExtractor.py
@@ -35,17 +35,26 @@ class FeatureExtractor:
         self.def_dict_p = import_feat(path.join(
             src.Utilities.point_breakdown_path, def_scoring_file_name))
 
+        # empty weekly stats dicts
         self.off_dict_emp = {}
         self.def_dict_emp = {}
         self.__fill_empty_dicts()
 
-    # TODO:
-    #       Add opponent to both feature dicts and injury status to the
-    #       offensive feature dict. Also add dome, turf, and location to both
-    #       feature dicts.
-    def extract(self, position, current_season_id):
+        # empty weekly feature
+        # [points, opponent, location, turf]
+        self.empty_feature_vector = []
+
+    def extract_feature(self, position, current_season_id):
         global positions
         src.Utilities.log('Entering extract method.', src.Utilities.extract_log)
+
+        # select game_info statement
+        statement = "select s.locationId, s.turf, g.homeTeam, g.awayTeam from "\
+                    "TeamLocations s, Games g where s.teamId = g.homeTeam " \
+                    "and g.seasonId = %s;" \
+
+        if not statement:
+            return []
 
         if position not in positions:
             src.Utilities.log('Error, invalid position.',
@@ -55,8 +64,9 @@ class FeatureExtractor:
         src.Utilities.log('Pulling data from the database.',
                           src.Utilities.extract_log)
 
-        results = self.__get_data(positions[position],
-                                  season_id=current_season_id)
+        results = self.__get_data('extract_statistics', [positions[position],
+                                                         current_season_id,
+                                                         None])
 
         if not results:
             src.Utilities.log('Empty result set.',
@@ -66,32 +76,51 @@ class FeatureExtractor:
                           src.Utilities.extract_log)
 
         feature_list = []
+        point_sum = 0
         current_feature_dict = self.__get_fresh_feature_dict(position)
         current_player = results[0][0]
 
         for line in results:
             if line[0] != current_player:
-                feature_list.append((current_player, current_feature_dict))
+                # this is going to be kind of slow slow.
+                res = self.__get_data('extract_game_info',
+                                      [current_season_id, current_player])
+                if not res:
+                    # either not currently on a team or couldn't pull from the
+                    # db, so who cares.
+                    continue
+                # feature vector [points, location, turf, opponent]
+
+                # going to change the stored proc to just return the opponent
+                if res[0][2] != res[0][4]:
+                    opp = res[0][2]
+                else:
+                    opp = res[0][3]
+                # instead of hashing, I will just give each team an id in the
+                #  range 1-32 and do something similar for the turf.
+                feature_vector = [point_sum, res[0][0], hash(res[0][1]),
+                                  hash(opp)]
+                feature_list.append((current_player, feature_vector))
+
+                point_sum = 0
                 current_feature_dict = self.__get_fresh_feature_dict(position)
                 current_player = line[0]
 
             if str(line[3]) in current_feature_dict:
                 if position == src.Utilities.Position.defense:
-                    current_feature_dict[str(line[3])] = float(line[4]) * \
-                                        self.def_dict_p[str(line[3])]
-                else:
-                    current_feature_dict[str(line[3])] = float(line[4]) * \
-                                        self.off_dict_p[str(line[3])]
+                    point_sum += float(line[4]) * self.def_dict_p[str(line[3])]
 
-        # print current_player
+                else:
+                    point_sum += float(line[4]) * self.off_dict_p[str(line[3])]
+
         src.Utilities.log('Exiting extract method.', src.Utilities.extract_log)
         return feature_list
 
     def __get_fresh_feature_dict(self, position):
         if position == src.Utilities.Position.defense:
-            return_dict = copy.deepcopy(self.def_dict_emp)
+            return_dict = self.def_dict_emp
         else:
-            return_dict = copy.deepcopy(self.off_dict_emp)
+            return_dict = self.off_dict_emp
 
         return return_dict
 
@@ -106,13 +135,15 @@ class FeatureExtractor:
             self.def_dict_emp[k] = 0.0
 
     @staticmethod
-    def __get_data(position, player_id=None, season_id=None):
-        return src.Utilities.execute_procedure('extract_statistics',
-                                               [position, season_id, player_id],
+    def __get_data(procedure, args):
+        return src.Utilities.execute_procedure(procedure,
+                                               args,
                                                src.Utilities.extract_log)
 
 if __name__ == '__main__':
     feature_extractor = FeatureExtractor()
 
-    for t in feature_extractor.extract(src.Utilities.Position.quarterback, 101):
+    for t in feature_extractor.extract_feature(
+            src.Utilities.Position.quarterback, 101):
+
         print t[0], t[1]
